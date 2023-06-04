7a12974c36cd5558eaaae0d4feb89ee2f30046f0
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2023-06-04 16:52:30 +0700

    added math expression support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hitungbudget/text_input.dart';
 import 'package:intl/intl.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Hitung Budget',
+      title: 'Hitung Budget',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Hitung Budget'),
+      home: const MyHomePage(title: 'Hitung Budget Harian Sampai Gajian'),
     );
   }
 }
@@ -37,36 +38,52 @@ class _MyHomePageState extends State<MyHomePage> {
   final DateTime today = DateTime.now();
   late DateTime tanggalGajian;
   final String _selamatDatang = "Selamat datang di penghitung budget :)";
+  final Parser _parser = Parser();
+  final ContextModel _contextModel = ContextModel();
   double _budgetHarian = 0;
   String _textResult = "";
-
+  bool _supportOperations = false;
 
   _MyHomePageState() {
     _textResult = _selamatDatang;
-    tanggalGajian = dateFormat.parse('25-${today.day < 25 ? today.month : today.month + 1}-${today.year}');
+    tanggalGajian = dateFormat.parse(
+        '25-${today.day < 25 ? today.month : today.month + 1}-${today.year}');
   }
 
   @override
   void initState() {
     super.initState();
     textEditingController.addListener(() {
-      setState(() {
-        _calculateBudget();
-      });
+      if (!_supportOperations) {
+        setState(() {
+          _calculateBudget();
+        });
+      }
     });
   }
 
   void _calculateBudget() {
-    int sisaHariMenujuGajian = (tanggalGajian.difference(today)).inDays + 1; // hanya bisa tepat apabila dilakukan jam 12 pas
+    int sisaHariMenujuGajian = (tanggalGajian.difference(today)).inDays +
+        1; // hanya bisa tepat apabila dilakukan jam 12 pas
     try {
-      _budgetHarian = int.parse(textEditingController.text) / sisaHariMenujuGajian;
+      if (_supportOperations) {
+        int parsedValue = _parser
+            .parse(textEditingController.text)
+            .evaluate(EvaluationType.REAL, _contextModel);
+        setState(() {
+          textEditingController.text = parsedValue.toString();
+        });
+      }
+      _budgetHarian =
+          int.parse(textEditingController.text) / sisaHariMenujuGajian;
       _textResult = "Budget harian sampai gajian ${_budgetHarian.toInt()}";
     } catch (_) {
       if (textEditingController.text.isEmpty) {
         _textResult = _selamatDatang;
         return;
       }
-      _textResult = "Inputnya bukan angka kakak :)";
+      _textResult =
+          "Inputnya bukan angka / kakak ingin pakai operasi matematika :)";
     }
   }
 
@@ -76,6 +93,17 @@ class _MyHomePageState extends State<MyHomePage> {
       _budgetHarian = 0;
       _textResult = _selamatDatang;
     });
+  }
+
+  void _onChangedOperation(bool? value) {
+    setState(() {
+      _supportOperations = value!;
+      _textResult = _selamatDatang;
+    });
+  }
+
+  void _onPressedButton() {
+    _calculateBudget();
   }
 
   @override
@@ -90,11 +118,52 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              "Masukkan uang anda hehehee :)",
+              "Masukkan uang anda hehehee :), btw inputnya support operasi matematika ya contoh: 100+750-20, tapi kamu harus centang dulu apakah mau support operasi, akan muncul tombol submit atau tekan enter agar operasi diproses",
+            ),
+            const Text(
+              "Asumsi gajian tanggal 25 yaa :)",
+            ),
+            const SizedBox(
+              height: 13,
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: _supportOperations,
+                  onChanged: _onChangedOperation,
+                ),
+                const Flexible(
+                  child: Text(
+                    "Support operasi",
+                  ),
+                ),
+              ],
             ),
             TextInput(
+              onSubmitted: (_) => _calculateBudget(),
               textEditingController: textEditingController,
               label: "Total uang anda",
+            ),
+            if (_supportOperations)
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                    onPressed: _onPressedButton,
+                    child: const Text(
+                      "Submit",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            const SizedBox(
+              height: 2.5,
             ),
             Text(
               _textResult,
--- lib/text_input.dart
+++ lib/text_input.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class TextInput extends StatefulWidget {
   final TextEditingController textEditingController;
   final String label;
-
-  const TextInput(
-      {required this.textEditingController, required this.label, super.key});
+  final Function(String) onSubmitted;
+  const TextInput({
+    required this.textEditingController,
+    required this.label,
+    required this.onSubmitted,
+    super.key,
+  });
 
   @override
   State<TextInput> createState() => _TextInputState();
@@ -15,6 +19,7 @@ class _TextInputState extends State<TextInput> {
   @override
   Widget build(BuildContext context) {
     return TextField(
+      onSubmitted: widget.onSubmitted,
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         label: Text(
