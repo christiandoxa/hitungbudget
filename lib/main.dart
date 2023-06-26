d3e31c1fe3b5409d2fa6df8d7aaae130cf882a9d
Christian Doxa Hamasiah <21328162+christiandoxa@users.noreply.github.com>
2023-06-26 07:48:19 +0700

    fixed holiday payday date bug

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
     int bulanGajian = today.day < 25 ? today.month : today.month + 1;
     String tanggalGajianText = tanggalGajianString(bulanGajian);
     tanggalGajian = dateFormat.parse(tanggalGajianText);
-    if (today.day < 25 &&
+    if (today.day > 22 && today.day < 25 &&
         (tanggalGajian.weekday == DateTime.sunday ||
             tanggalGajian.weekday == DateTime.saturday)) {
       bulanGajian = today.month + 1;
